2d04238b144ad60c9b017ffca87e1e4acf584cfe
ryanrych <ryanjrychlak@gmail.com>
2022-02-01 00:11:55 -0500

    Added algorithm to make the statistically best choices to guess a random word in 6 guesses.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from random import choice
+
 file = open("words.txt", "r")
 words = []
 for line in file:
@@ -22,5 +24,70 @@ for word in words:
             score += letterScores[word[i]]
             taken.append(word[i])
 
-    wordScores[word] = score
+    wordScores[word] = [score, False]
+wordScores = dict(reversed(sorted(wordScores.items(), key=lambda item: item[1][0])))
 
+
+target = choice(words)
+requirements = {"solved":['*','*','*','*','*'], "contains":[], "out":[]}
+correct = False
+for i in range(6):
+    guess = ""
+
+    for word in wordScores:
+
+        if wordScores[word][1]:
+            continue
+
+        flag = True
+
+        for j in range(len(requirements["out"])):
+            if requirements["out"][j] in word:
+                flag = False
+                break
+
+        if not flag:
+            continue
+
+        for j in range(5):
+            if word[j] != requirements["solved"][j] and requirements["solved"][j] != '*':
+                flag = False
+                break
+
+        if not flag:
+            continue
+
+        for j in range(len(requirements["contains"])):
+            if not requirements["contains"][j] in word:
+                flag = False
+                break
+
+        if not flag:
+            continue
+
+        guess = word
+        wordScores[word][1] = True
+        break
+
+    print("Guessed " + guess)
+
+    if guess == target:
+        print("Guessed " + target + " in " + str(i + 1) + " guesses!")
+        correct = True
+        break
+
+    for j in range(len(guess)):
+        if guess[j] == target[j]:
+            requirements["solved"][j] = guess[j]
+            print("Letter " + guess[j] + " in correct spot")
+
+        elif guess[j] in target:
+            requirements["contains"].append(guess[j])
+            print("Letter " + guess[j] + " in word")
+
+        else:
+            requirements["out"].append(guess[j])
+
+
+if not correct:
+    print("couldn't guess " + target)
